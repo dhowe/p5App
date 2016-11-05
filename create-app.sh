b1237d5f8366cc4cc001f2563f43d1026baf7882
dhowe <github.utensil@mailhero.io>
2016-11-05 20:18:35 +0800

    updated, working osx

--- create-app.sh
+++ create-app.sh
@@ -2,8 +2,19 @@
 
 # 1. run your program in eclipse
 # 2. run this script: ./create-app.sh
-# 3. run the AppRunner/AppRunner.pde skectch in this directory from Processing
-# 4. choose file->export application from Processing menu
+# 3. run the AppRunner/AppRunner.pde sketch in this directory from Processing
+# 4. choose file->'export application' from Processing menu
 # 5. test the exported application
 
-cd bin && jar cvf ../app.jar * && cd -
+PROJECT_BIN=${1:-~/Documents/Projects/RELC3/bin}
+
+mkdir tmp
+
+cp -r ${PROJECT_BIN}/* tmp/
+cd tmp && jar cvf ../app.jar * && cd -
+cp app.jar AppRunner/code/
+cp rita.jar AppRunner/code/
+ls AppRunner/code/
+
+
+rm -rf tmp
